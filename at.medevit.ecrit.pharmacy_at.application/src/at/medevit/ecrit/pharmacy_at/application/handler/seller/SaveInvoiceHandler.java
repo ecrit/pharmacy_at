d2cb4264cfc381b79d5e2dda67c4120589a27433
luciaamann <lucia.amann@students.fhv.at>
2014-01-13 17:04:01 +0100

    rename

--- at.medevit.ecrit.pharmacy_at.application/src/at/medevit/ecrit/pharmacy_at/application/handler/seller/SaveInvoiceHandler.java
+++ at.medevit.ecrit.pharmacy_at.application/src/at/medevit/ecrit/pharmacy_at/application/handler/seller/SaveInvoiceHandler.java
@@ -4,7 +4,7 @@ package at.medevit.ecrit.pharmacy_at.application.handler.seller;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 
-public class AddInvoiceHandler {
+public class SaveInvoiceHandler {
 	@Execute
 	public void execute() {
 		//TODO Your code goes here
