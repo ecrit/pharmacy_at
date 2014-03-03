5f1b8d5ba538cf82450c053d0615eb533cc9db33
lamann <amann@medevit.at>
2014-03-03 16:28:20 +0100

    sample model update

--- at.medeit.ecrit.pharmacy_at.core/src/at/medevit/ecrit/pharmacy_at/core/SampleModel.java
+++ at.medeit.ecrit.pharmacy_at.core/src/at/medevit/ecrit/pharmacy_at/core/SampleModel.java
@@ -16,26 +16,21 @@ import at.medevit.ecrit.pharmacy_at.model.Report;
 import at.medevit.ecrit.pharmacy_at.model.Stock;
 import at.medevit.ecrit.pharmacy_at.model.StockArticle;
 import at.medevit.ecrit.pharmacy_at.model.StockOrder;
-import at.medevit.ecrit.pharmacy_at.model.StockOrderStatus;
 import at.medevit.ecrit.pharmacy_at.model.impl.ModelPackageImpl;
 
+//this is the sample model
 public class SampleModel {
 	static ModelFactory factory = ModelFactory.eINSTANCE;
 	static EMFModelLoad loader = new EMFModelLoad();
-// static Resource resource = null;
 	static Pharmacy pharmacy = null;
 	
 	static StockOrder currOrder;
 	private static Invoice curInvoice;
 	
-	public static void initSampleModel(){
-		ModelPackageImpl.init();
-		pharmacy = loader.load();
-	}
-	
-	private static Pharmacy getPharmacy(){
+	public static Pharmacy getPharmacy(){
 		if (pharmacy == null) {
-			initSampleModel();
+			ModelPackageImpl.init();
+			pharmacy = loader.load();
 		}
 		return pharmacy;
 	}
@@ -44,95 +39,67 @@ public class SampleModel {
 		return getPharmacy().getStock();
 	}
 	
-	public static List<Invoice> getAllInvoices(){
-		return pharmacy.getInvoices();
-	}
-	
-	public static void saveInvoice(Invoice invoice){
-		pharmacy.getInvoices().add(invoice);
-		curInvoice = null;
-		loader.save();
-	}
-	
-	public static Invoice getCurrentInvoice(){
+	public static Invoice getInvoice(){
 		if (curInvoice == null) {
 			initNewInvoice();
 		}
 		return curInvoice;
 	}
 	
-	private static void initNewInvoice(){
-		Invoice i1 = factory.createInvoice();
-		i1.setDate(Calendar.getInstance().getTime());
-		Random rnd = new Random();
-		i1.setId(rnd.nextInt(Integer.MAX_VALUE));
-		
-		curInvoice = i1;
+	public static StockOrder getStockOrder(){
+		if (currOrder == null) {
+			initNewStockOrderInstance();
+		}
+		return currOrder;
 	}
 	
-	public static void revertCurrentInvoice(){
-		Invoice i = getCurrentInvoice();
+	public static Article getValidArticleCopy(Article a){
+		Article copy = EcoreUtil.copy(a);
+		pharmacy.getLineItems().getArticle().add(copy);
+		return copy;
+	}
+	
+	public static List<Article> getNotYetPrescriptedArticle(){
+		List<Article> notPrescripted = new ArrayList<Article>();
+		List<Article> prescripted = new ArrayList<Article>();
 		
-		for (Article article : i.getArticle()) {
-			for (StockArticle stockArticle : getStock().getArticles()) {
-				// find stock article on list and reset number on stock
-				if (stockArticle.getArticle().getName().equals(article.getName())) {
-					stockArticle.setNumberOnStock(stockArticle.getNumberOnStock() + 1);
-				}
+		// get all articles that have a prescription
+		for (Prescription p : getInvoice().getPrescription()) {
+			prescripted.addAll(p.getArticle());
+		}
+		
+		// add only articles that have no prescription yet
+		for (Article article : getInvoice().getArticle()) {
+			if (!prescripted.contains(article)) {
+				notPrescripted.add(article);
 			}
 		}
-		pharmacy.getInvoices().remove(i);
-		loader.save();
+		return notPrescripted;
 	}
 	
-	public static void addArticleToInvoice(StockArticle stockArticle){
-		getCurrentInvoice().getArticle().add(getValidArticleCopy(stockArticle.getArticle()));
+	public static List<Prescription> getAllPrescriptions(){
+		List<Prescription> prescriptions = new ArrayList<>();
+		for (int i = 0; i < pharmacy.getInvoices().size(); i++) {
+			prescriptions.addAll(pharmacy.getInvoices().get(i).getPrescription());
+		}
+		return prescriptions;
 	}
 	
-	public static StockOrder getStockOrderInstance(){
-		currOrder = factory.createStockOrder();
-		Random rnd = new Random();
-		currOrder.setNumber(rnd.nextInt(Integer.MAX_VALUE));
-		
-		return currOrder;
-	}
-	
-	public static StockOrder getCurrentStockOrder(){
-		return currOrder;
-	}
-	
-	public static void addStockOrder(StockOrder order){
-		pharmacy.getStockOrders().add(order);
-		
-		loader.save();
-	}
-	
-	public static List<StockOrder> getAllStockOrders(){
-		return pharmacy.getStockOrders();
-	}
-	
-	public static void stockOrderReceived(StockOrder order){
-		int orderIdx = pharmacy.getStockOrders().indexOf(order);
-		pharmacy.getStockOrders().get(orderIdx).setStatus(StockOrderStatus.RECEIVED);
-		
+	public static void addInvoice(Invoice invoice){
+		pharmacy.getInvoices().add(invoice);
+		curInvoice = null;
 		loader.save();
 	}
 	
 	public static void addReport(Report report){
 		pharmacy.getReports().add(report);
-		
 		loader.save();
 	}
 	
-	public static Report getLatestReport(){
-		if (pharmacy.getReports().size() == 0) {
-			return null;
-		}
-		return pharmacy.getReports().get(pharmacy.getReports().size() - 1);
-	}
-	
-	public static void revertReport(Report report){
-		pharmacy.getReports().remove(report);
+	public static void addStockOrder(StockOrder order){
+		pharmacy.getStockOrders().add(order);
+		currOrder = null;
+		loader.save();
 	}
 	
 	public static boolean addToStock(StockArticle newStockArticle){
@@ -149,63 +116,31 @@ public class SampleModel {
 		return true;
 	}
 	
-	public static void addPrescription(Prescription p){
-		getCurrentInvoice().getPrescription().add(p);
-	}
-	
-	/**
-	 * Adds prescription and assures synch with invoice data
-	 * 
-	 * @param p
-	 *            Prescription to add
-	 */
-	public static void addPrescriptionAndSync(Prescription p){
-		getCurrentInvoice().getPrescription().add(p);
-		synchPrescriptedArticlesWithInvoice(p);
-	}
-	
-	/**
-	 * in case article was added to prescription directly -> add to invoice articles as well
-	 * 
-	 * @param p
-	 *            Prescription that was added
-	 */
-	private static void synchPrescriptedArticlesWithInvoice(Prescription p){
-		List<Article> onInvoice = getCurrentInvoice().getArticle();
-		List<Article> onPrescription = p.getArticle();
+	private static void initNewInvoice(){
+		Invoice i1 = factory.createInvoice();
+		i1.setDate(Calendar.getInstance().getTime());
+		Random rnd = new Random();
+		i1.setId(rnd.nextInt(Integer.MAX_VALUE));
 		
-		for (Article a : onPrescription) {
-			if (!onInvoice.contains(a)) {
-				getCurrentInvoice().getArticle().add(a);
-			}
-		}
+		curInvoice = i1;
 	}
 	
-	public static List<Prescription> getAllPrescriptionsForCurrentInvoice(){
-		return getCurrentInvoice().getPrescription();
+	private static void initNewStockOrderInstance(){
+		currOrder = factory.createStockOrder();
+		Random rnd = new Random();
+		currOrder.setNumber(rnd.nextInt(Integer.MAX_VALUE));
 	}
 	
-	public static void addArticleToPrescription(Prescription prescription, StockArticle stockArticle){
-		prescription.getArticle().add(getValidArticleCopy(stockArticle.getArticle()));
-	}
-	
-	public static void deletePrescription(Prescription prescription){
-		getCurrentInvoice().getPrescription().remove(prescription);
-	}
-	
-	public static void removeArticleFromPrescription(Prescription prescription, Article a){
-		prescription.getArticle().remove(a);
+	public static void revertCurrentInvoice(){
+		Invoice i = getInvoice();
 		
-	}
-	
-	public static void removeArticleFromInvoice(Article a){
-		for (Article arti : getCurrentInvoice().getArticle()) {
-			if (arti.getName().equals(a.getName())) {
-				getCurrentInvoice().getArticle().remove(arti);
-				resetNumberOnStock(a);
-				break;
-			}
+		for (Article article : i.getArticle()) {
+			resetNumberOnStock(article);
+			removeLineItem(article);
 		}
+		pharmacy.getInvoices().remove(i);
+		curInvoice = null;
+		loader.save();
 	}
 	
 	private static void resetNumberOnStock(Article a){
@@ -216,9 +151,32 @@ public class SampleModel {
 		}
 	}
 	
+	public static void removeArticleFromInvoice(Article a){
+		for (Article arti : getInvoice().getArticle()) {
+			if (arti.getName().equals(a.getName())) {
+				getInvoice().getArticle().remove(arti);
+				resetNumberOnStock(a);
+				removeLineItem(a);
+				break;
+			}
+		}
+	}
+	
+	public static void removeFromStock(StockArticle selection){
+		getStock().getArticles().remove(selection);
+	}
+	
+	private static void removeLineItem(Article a){
+		pharmacy.getLineItems().getArticle().remove(a);
+	}
+	
+	public static void removeReport(Report report){
+		pharmacy.getReports().remove(report);
+	}
+	
 	public static boolean isSingleInvoiceArticle(Article a){
 		int counter = 0;
-		for (Article arti : getCurrentInvoice().getArticle()) {
+		for (Article arti : getInvoice().getArticle()) {
 			if (arti.getName().equals(a.getName())) {
 				counter++;
 			}
@@ -229,53 +187,7 @@ public class SampleModel {
 		return false;
 	}
 	
-	/**
-	 * compares all invoice articles with all prescripted articles
-	 * 
-	 * @return only unprescripted articles (empty list if all are prescripted)
-	 */
-	public static List<Article> getNotYetPrescriptedArticle(){
-		List<Article> notPrescripted = new ArrayList<Article>();
-		List<Article> prescripted = new ArrayList<Article>();
-		
-		// get all articles that have a prescription
-		for (Prescription p : SampleModel.getAllPrescriptionsForCurrentInvoice()) {
-			prescripted.addAll(p.getArticle());
-		}
-		
-		// add only articles that have no prescription yet
-		for (Article article : getCurrentInvoice().getArticle()) {
-			if (!prescripted.contains(article)) {
-				notPrescripted.add(article);
-			}
-		}
-		return notPrescripted;
-	}
-	
-	public static void deleteFromStock(StockArticle selection){
-		getStock().getArticles().remove(selection);
-	}
-	
-	public static List<Prescription> getAllPrescriptions(){
-		List<Prescription> prescriptions = new ArrayList<>();
-		for (int i = 0; i < pharmacy.getInvoices().size(); i++) {
-			prescriptions.addAll(pharmacy.getInvoices().get(i).getPrescription());
-		}
-		return prescriptions;
-	}
-	
-	public static Article getValidArticleCopy(Article a){
-		Article copy = EcoreUtil.copy(a);
-		pharmacy.getLineItems().getArticle().add(copy);
-		return copy;
-	}
-	
-	public static List<Article> getAllLineItems(){
-		return pharmacy.getLineItems().getArticle();
-	}
-	
-	public static void updateArticleList(){
+	public static void update(){
 		loader.save();
-		
 	}
 }
