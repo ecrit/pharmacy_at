d07396aef25e2257ce97f8299ec18124fd08ae70
lamann <amann@medevit.at>
2014-06-27 09:26:32 +0200

    Included a fragment so we can test this case as well

---
+++ at.medevit.ecrit.pharmacy_at.application/src/at/medevit/ecrit/pharmacy_at/application/handler/AboutHandler.java
@@ -0,0 +1,20 @@
+package at.medevit.ecrit.pharmacy_at.application.handler;
+
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class AboutHandler {
+	@Execute
+	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell){
+		MessageDialog
+			.openInformation(
+				shell,
+				"About",
+				"This is the PharmAT demo application created during the Ecrit project!\nFor further information see: www.ecrit.at");
+	}
+	
+}
